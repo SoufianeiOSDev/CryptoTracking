05a7242bc811183442fcceebe673737f86d4a3cc
soufiane <sofy.ben@icloud.com>
2022-01-13 16:10:49 +0000

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Soufiane Benkhaldoun on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
